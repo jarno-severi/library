671f62e201f26d6cfbcc4ec0efb88fd6f12ede9d
Jarno Severi <jarno.severi@gmail.com>
2021-08-16 19:50:09 +0300

    navigation, images, form validation etc

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-function toggleForm () {
+function toggleForm() {
     const element = document.querySelector('#form');
     element.toggleAttribute('hidden');
-    openForm.toggleAttribute('hidden');
 }
 
 const openForm = document.querySelector('#open-button');
@@ -20,8 +19,8 @@ function Book(index, title, author, pages, read) {
 }
 
 Book.prototype.info = function () {
-    if (this.read) return `${this.title}\nby ${this.author}\n\nPages: ${this.pages}\n\nYou have read this book!`
-    else return `${this.title}\nby ${this.author}\n\nPages: ${this.pages}\n\nYou have not read this book.`
+    if (this.read) return `${this.title}\nby ${this.author}\nPages: ${this.pages}\nRead: Yes`
+    else return `${this.title}\nby ${this.author}\nPages: ${this.pages}\nRead: No`
 }
 
 Book.prototype.toggleRead = function () {
@@ -29,56 +28,71 @@ Book.prototype.toggleRead = function () {
     if (this.read === true) return this.read = false;
 }
 
+function validateForm() {
+    const form = document.querySelector('form');
+    let error = 0;
+
+    for (let i = 0; i < form.length; i++) {
+        if (form[i].value === "") {
+            form[i].placeholder = "* Please fill out this field";
+            error++;
+        }
+    }
+
+    if (error === 0) return addBookToLibrary();
+    else return false;
+}
+
 let index = 0; // Counter for added books
 
 function addBookToLibrary() {
 
-    let book = new Book (
-        index, 
-        inputTitle.value, 
-        inputAuthor.value, 
-        inputPages.value, 
-        inputRead.checked
+        let book = new Book(
+            index,
+            inputTitle.value,
+            inputAuthor.value,
+            inputPages.value,
+            inputRead.checked
         );
 
-    index++;
-    myLibrary.push(book); // Push book object to "library" (array)
+        index++;
+        myLibrary.push(book); // Push book object to "library" (array)
 
-    // Creates a div for the book info
-    let div = document.createElement('div');
-    div.classList.add('card');
-    container.appendChild(div);
+        // Creates a div for the book info
+        let div = document.createElement('div');
+        div.classList.add('card');
+        container.appendChild(div);
 
-    // Select newly created card
-    const card = container.lastChild;  
+        // Select newly created card
+        const card = container.lastChild;
 
-    // Add book info into this div
-    card.textContent = book.info(); 
+        // Add book info into this div
+        card.textContent = book.info();
 
-    // Bind book index to this div to be able to removed by the user
-    card.setAttribute('data-index', book.index);
+        // Bind book index to this div to be able to removed by the user
+        card.setAttribute('data-index', book.index);
 
-    // Create read button for the book
-    const readBtn = document.createElement('BUTTON');
-    readBtn.textContent = "Read this book";
-    readBtn.classList.add('read');
-    card.appendChild(readBtn);
+        // Create read button for the book
+        const readBtn = document.createElement('BUTTON');
+        readBtn.textContent = "Change read status";
+        readBtn.classList.add('read');
+        card.appendChild(readBtn);
 
-    // Add evenlistener for the read button
-    const buttonRead = card.querySelector('.read');
-    buttonRead.addEventListener('click', toggleRead);
+        // Add evenlistener for the read button
+        const buttonRead = card.querySelector('.read');
+        buttonRead.addEventListener('click', toggleRead);
 
-    // Create remove button for the book
-    const removeBtn = document.createElement('BUTTON');
-    removeBtn.textContent = "Remove this book";
-    removeBtn.classList.add('remove');
-    card.appendChild(removeBtn);
+        // Create remove button for the book
+        const removeBtn = document.createElement('BUTTON');
+        removeBtn.textContent = "Remove this book";
+        removeBtn.classList.add('remove');
+        card.appendChild(removeBtn);
 
-    // Add evenlistener for the remove button
-    const buttonRemove = card.querySelector('.remove');
-    buttonRemove.addEventListener('click', removeBook);
-    
-    resetForm();
+        // Add evenlistener for the remove button
+        const buttonRemove = card.querySelector('.remove');
+        buttonRemove.addEventListener('click', removeBook);
+
+        resetForm();
 }
 
 // Creates a div element for book info
@@ -89,7 +103,7 @@ function createCard() {
 }
 
 
-function removeBook () {
+function removeBook() {
     let bookIndex = this.parentNode.dataset.index;
     let pos = myLibrary.map(obj => obj.index).indexOf(+bookIndex);
     myLibrary.splice(pos, 1);
@@ -97,10 +111,9 @@ function removeBook () {
     myLibrary.forEach(book => createCards(book));
 }
 
-function toggleRead () {
+function toggleRead() {
     let bookIndex = this.parentNode.dataset.index;
     let pos = myLibrary.map(obj => obj.index).indexOf(+bookIndex);
-    console.log(pos);
     myLibrary[pos].toggleRead();
     clearCards();
     myLibrary.forEach(book => createCards(book));
@@ -117,7 +130,7 @@ function createCards(book) {
     firstDiv.setAttribute('data-index', book.index);
 
     let readBtn = document.createElement('BUTTON');
-    readBtn.textContent = "Read this book";
+    readBtn.textContent = "Change read status";
     readBtn.classList.add('read');
     firstDiv.appendChild(readBtn);
 
@@ -133,7 +146,7 @@ function createCards(book) {
     remove.addEventListener('click', removeBook);
 }
 
-function resetForm () {
+function resetForm() {
     document.querySelector('#add-book').reset();
 }
 
@@ -152,4 +165,4 @@ const inputRead = document.querySelector('input[name="read"');
 const container = document.querySelector('.container');
 
 const addBookButton = document.querySelector('#submit');
-addBookButton.addEventListener('click', addBookToLibrary);
+addBookButton.addEventListener('click', validateForm);
